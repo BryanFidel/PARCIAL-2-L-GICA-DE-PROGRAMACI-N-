923b157d8b48240377f256f743af221548757d8b
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-04-05 22:19:39 -0500

    ingreso de los aportes de los socios.

--- natillerapunto2/Program.cs
+++ natillerapunto2/Program.cs
@@ -18,7 +18,29 @@ namespace natillerapunto2
                 int multasSocio1 = 0;
                 int multasSocio2 = 0;
 
-               
+                Console.WriteLine("Ingrese los aportes mensuales del primer socio:");
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.Write($"Mes {i + 1}: $");
+                    double aporte = Convert.ToDouble(Console.ReadLine());
+                    if (aporte == 0)
+                        multasSocio1++;
+                    else
+                        ahorrosSocio1[i] = aporte;
+                }
+
+                Console.WriteLine("Ingrese los aportes mensuales del segundo socio:");
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.Write($"Mes {i + 1}: $");
+                    double aporte = Convert.ToDouble(Console.ReadLine());
+                    if (aporte == 0)
+                        multasSocio2++;
+                    else
+                        ahorrosSocio2[i] = aporte;
+                }
+
+        
             }
         }
     }
