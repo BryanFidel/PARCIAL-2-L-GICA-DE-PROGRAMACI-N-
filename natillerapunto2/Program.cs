984748950d9ddc92d1e275bf214f6c1fe532b45f
Bryan Fidel Huaynalaya Rios <brayanfid@gmail.com>
2024-04-05 22:14:08 -0500

    ingresar variables basicas de la natillera

---
+++ natillerapunto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace natillerapunto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double TasaInteresMensual = 0.025;
+            const double MultaNoAporte = 20000;
+
+            static void Main(string[] args)
+            {
+                double[] ahorrosSocio1 = new double[12];
+                double[] ahorrosSocio2 = new double[12];
+                double prestamoSocio1 = 0;
+                double prestamoSocio2 = 0;
+                int multasSocio1 = 0;
+                int multasSocio2 = 0;
+
+               
+            }
+        }
+    }
+}
+
